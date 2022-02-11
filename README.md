32ee19f5d67e96abbb74c1f90cd38603927e0d5d
amytnyk <alex.mytnyk@ukr.net>
2022-02-12 00:17:14 +0200

    final release

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-## TODO
+# Geofilm
+#### Description
+Python application that displays 10 nearest filming locations on the map and visualizes shortest path you need to travel through every location.
+#### Installation
+Clone repository and install requirements.txt
+```bash
+pip -r requirements.txt
+```
+#### Usage
+```bash
+python main.py <year> <latitude> <longitude> <path_to_dataset>
+```
+This will generate map.html file where 10 nearest filming locations of given year from given dataset will be displayed. Moreover, it will contain shortest path between all of those locations.
+#### Example Output
+![Example](assets/example.png)
--- main.py
+++ main.py
@@ -3,12 +3,18 @@ Module for displaying the nearest 10 filming locations on the interactive map
 """
 import json
 import os.path
+import six
 import sys
+import argparse
 from math import sin, cos, pi, sqrt, atan2
 from typing import List, Tuple, Optional, Dict
 from geopy.geocoders import Nominatim
 from geopy.extra.rate_limiter import RateLimiter
-from folium import Map, FeatureGroup, Marker, Icon
+from folium import Map, FeatureGroup, Marker, Icon, LayerControl
+import folium.vector_layers
+
+sys.modules['sklearn.externals.six'] = six
+import mlrose as mlrose
 
 
 def read_dataset(path: str, year: int) -> List[Tuple[str, str]]:
@@ -16,7 +22,7 @@ def read_dataset(path: str, year: int) -> List[Tuple[str, str]]:
     Returns dataset as list of tuples (film name, filming location)
     For example:
     """
-    with open(path, 'r') as file:
+    with open(path, 'r', encoding='ISO-8859-1') as file:
         dataset = []
         lines = file.read().split('\n')
         for line in lines[lines.index("LOCATIONS LIST") + 2:-2]:
@@ -24,12 +30,21 @@ def read_dataset(path: str, year: int) -> List[Tuple[str, str]]:
             if line[last_tab_index + 1] == '(':
                 line = line[:last_tab_index]
             location = line[line.rindex('\t') + 1:]
-            name = line[:line.index('\t')]
-            line = line[:line.index('\t')]
-            if line[-1] == '}':
+            line = line[:line.rindex('\t')]
+
+            if '}' in line:
                 line = line[:line.index('{') - 1]
+            if '\t' in line:
+                line = line[:line.index('\t')]
+            if '(TV)' in line:
+                line = line[:line.rindex('(TV)')]
+            if '(V)' in line:
+                line = line[:line.rindex('(V)')]
+            line = line.strip()
+
+            name = line
             film_year = line[line.rindex(' '):][2:-1]
-            if int(film_year) == year:
+            if film_year.isdigit() and int(film_year) == year:
                 dataset.append((name, location))
         return dataset
 
@@ -72,7 +87,7 @@ def display_progress(progress: Tuple[str, int, int]):
     sys.stdout.flush()
 
 
-def get_films_with_coordinates(dataset: List[Tuple[str, str]]) ->\
+def get_films_with_coordinates(dataset: List[Tuple[str, str]]) -> \
         List[Tuple[str, Tuple[float, float]]]:
     """
     Returns films with coordinates from films with location strings
@@ -100,6 +115,8 @@ def distance(point1: Tuple[float, float], point2: Tuple[float, float]) -> float:
     Computes distance between two points
     >>> distance((54, 21), (53, 19))
     172797.4514739205
+    >>> distance((54, 21), (-41, 174))
+    172797.4514739205
     """
     r = 6371e3
     phi1 = point1[0] * pi / 180
@@ -116,20 +133,43 @@ def get_nearest_locations(dataset: List[Tuple[str, Tuple[float, float]]],
     """
     Returns top 10 nearest locations
     """
-    return sorted(dataset, key=lambda x: distance(x[1], location))[-10:]
+    return sorted(dataset, key=lambda x: distance(x[1], location))[:10]
+
+
+def get_shortest_path(coordinates: List[Tuple[float, float]]) -> List[int]:
+    distances = []
+    for idx1, coord1 in enumerate(coordinates):
+        for idx2, coord2 in enumerate(coordinates):
+            if idx1 < idx2:
+                distances.append((idx1, idx2, distance(coord1, coord2)))
+
+    fitness_dists = mlrose.TravellingSales(distances=distances)
+    problem_fit = mlrose.TSPOpt(length=len(coordinates), fitness_fn=fitness_dists, maximize=False)
+    return mlrose.genetic_alg(problem_fit, mutation_prob=0.2, max_attempts=50, random_state=2)[0]
 
 
 def get_map(dataset: List[Tuple[str, Tuple[float, float]]], location: Tuple[float, float]) -> Map:
     """
-    Creates map with given list of points in dataset
+    Returns map with given list of points in dataset
     """
-    html_map = Map(location=location, zoom_start=10)
-    fg = FeatureGroup(name="Filming locations")
+    html_map = Map(location=location, zoom_start=5)
 
+    points_fg = FeatureGroup(name="Filming locations")
     for entry in dataset:
-        fg.add_child(Marker(location=entry[1], popup=entry[-0], icon=Icon()))
+        points_fg.add_child(Marker(location=entry[1], popup=entry[0], icon=Icon()))
+    html_map.add_child(points_fg)
 
-    html_map.add_child(fg)
+    lines_fg = FeatureGroup(name="Shortest Path")
+
+    all_coords = [location] + list(map(lambda x: x[1], dataset))
+    path = list(map(lambda x: all_coords[x], get_shortest_path(all_coords)))
+    lines_fg.add_child(folium.vector_layers.PolyLine(locations=path + [path[0]], weight=5))
+    lines_fg.add_child(Marker(location=location, popup="You are here", icon=Icon(color='green')))
+
+    html_map.add_child(lines_fg)
+
+    html_map.add_child(LayerControl())
+
     return html_map
 
 
@@ -140,4 +180,19 @@ def create_map(path_to_dataset: str, location: Tuple[float, float], year: int):
     html_map.save('map.html')
 
 
-create_map("locations_small.list", (54, 21), 2014)
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("year", help="movie year",
+                        type=int)
+    parser.add_argument("lat", help="Latitude",
+                        type=float)
+    parser.add_argument("lng", help="Longitude",
+                        type=float)
+    parser.add_argument("path", help="database file path",
+                        type=str)
+    args = parser.parse_args()
+    create_map(args.path, (args.lat, args.lng), args.year)
+
+
+if __name__ == "__main__":
+    main()
